8f60e7f17b038f58aefbb6dbf5eccfcef82338d1
lenjoy <36353485+lenjoy@users.noreply.github.com>
2018-03-29 15:32:01 -0700

    Update date_sample.py

--- python/date_sample.py
+++ python/date_sample.py
@@ -2,6 +2,10 @@ from datetime import datetime, timedelta
 
 # check a time window
 lookback_days = 28
-date_N_days_ago = datetime.now() - timedelta(days=lookback_days)
 
-start_dt = date_N_days_ago.strftime('%Y-%m-%d')
+now = datetime.now()
+date_N_days_ago = now - timedelta(days=lookback_days)
+start_dt_str = date_N_days_ago.strftime('%Y-%m-%d')
+
+given_day = datetime.strptime('2018-03-15', '%Y-%m-%d')
+print((now - given_day).days)
