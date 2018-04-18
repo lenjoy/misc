ba291ad1f7ac7872f8b4ffd371de1cfcf7bba2bf
lenjoy <36353485+lenjoy@users.noreply.github.com>
2018-04-17 21:29:11 -0700

    Update date_sample.py

--- python/date_sample.py
+++ python/date_sample.py
@@ -7,5 +7,5 @@ now = datetime.now()
 date_N_days_ago = now - timedelta(days=lookback_days)
 start_dt_str = date_N_days_ago.strftime('%Y-%m-%d')
 
-given_day = datetime.strptime('2018-03-15', '%Y-%m-%d')
+given_day = datetime.strptime('2017-03-29 12:05:30.0', '%Y-%m-%d %H:%M:%S.%f')
 print((now - given_day).days)
