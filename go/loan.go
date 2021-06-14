af75b54a4f7d32b943803f8fc6be56956c8b8001
lenjoy <36353485+lenjoy@users.noreply.github.com>
2021-06-13 20:23:23 -0700

    Update loan.go

--- go/loan.go
+++ go/loan.go
@@ -1,6 +1,4 @@
 /*
-Amortization Schedule:
-
 final month: a0*r + a0 == C
 
 (a0+a1) * r + a1 == C
@@ -13,14 +11,13 @@ final month: a0*r + a0 == C
   ==>  a2 * (r+1) == a1
 
 
-1 + a^1 + a^2 + ... + a^(n-1) == S
+b == 1/(r+1)
+
+1 + b^1 + b^2 + ... + b^(n-1) == S
   ==>
-    a^1 + a^2 + ... + a^(n-1) + a^n == S*a
-  ==>  1 - a^n == (1-a)*S
-  ==>  S == (a^n - 1) / (a-1)
-
-  a == 1/(r+1)
-
+    b^1 + b^2 + ... + b^(n-1) + b^n == S*b
+  ==>  1 - b^n == (1-b)*S
+  ==>  S == (b^n - 1) / (b-1)
 */
 
 package main
@@ -30,29 +27,32 @@ import (
 	"math"
 )
 
-func compound(n, r float64) float64 {
-	a := 1.0 / (r + 1.0)
-	return (a - 1) / (math.Pow(a, n) - 1.0)
+func geometricSum(n, r float64) float64 {
+	b := 1.0 / (r + 1.0)
+	return (b - 1) / (math.Pow(b, n) - 1.0)
 }
 
-func main() {
-	yearTotal := 30
-	a0 := 1000000.0
-	n := yearTotal * 12
-	r := 0.025 / 12.0
-
-	principal := a0 * compound(float64(n), r)
-	sumPrincipal := principal
+func amortization(yearTotal int, principalTotal, r float64) {
+	principal := principalTotal * geometricSum(float64(yearTotal*30), r)
+	sumPrincipal := 0.0
 
 	for year := yearTotal; year > 0; year-- {
 		for month := 12; month > 0; month-- {
-			if year == yearTotal && month == 12 {
-			} else {
+			if year != yearTotal || month != 12 {
+				// no need for the last month
 				principal /= (1 + r)
-				sumPrincipal += principal
 			}
+			sumPrincipal += principal
 			interest := sumPrincipal * r
 			fmt.Printf("yy=%d, mm=%d, total=%.2f (principal=%.2f + interest=%.2f)\n", year, month, principal+interest, principal, interest)
 		}
 	}
+}
+
+func main() {
+	yearTotal := 30
+	principalTotal := 1000000.0
+	rateMonthly := 0.03 / 12.0
+
+	amortization(yearTotal, principalTotal, rateMonthly)
 }
